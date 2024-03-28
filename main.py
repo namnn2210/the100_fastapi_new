9dcfab5751a019329cd5055e8777295864efbd66
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-03-28 21:19:58 +0700

    edit

--- main.py
+++ main.py
@@ -4,9 +4,25 @@ from fastapi import FastAPI
 from database.database import db_engine
 from model import models
 from router import category, supplier
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=db_engine)
+
+origins = [
+    "http://the100.vn",
+    "https://the100.vn",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     app.include_router(category.router, prefix="/api")
