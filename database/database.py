cf360bab11250c36ce3da11f046f83654a60191f
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-03-28 21:22:00 +0700

    edit

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqldb://root:@localhost/the100"
+DATABASE_URL = "mysql+mysqldb://root:ngocnam2210@localhost/the100"
 
 db_engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
