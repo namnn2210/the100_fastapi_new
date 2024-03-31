53a1ec9095134bf798db318bac06c6ddab4286fb
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-03-31 13:47:26 +0700

    edit

--- repository/category_repository.py
+++ repository/category_repository.py
@@ -8,30 +8,54 @@ class CategoryRepository(Repository):
 
     def create(self, **kwargs) -> Category:
         category = Category(**kwargs)
-        self.db.add(category)
-        self.db.commit()
-        self.db.refresh(category)
-        return category
+        try:
+            self.db.add(category)
+            self.db.commit()
+            self.db.refresh(category)
+            return category
+        except SQLAlchemyError as e:
+            self.db.rollback()
+            print("An error occurred while creating the category:", str(e))
+            return category
 
     def find(self, id: int) -> Optional[Category]:
-        return self.db.query(Category).filter(Category.id == id).first()
+        try:
+            return self.db.query(Category).filter(Category.id == id).first()
+        except SQLAlchemyError as e:
+            print("An error occurred while finding the category:", str(e))
+            return None
 
     def find_all(self) -> List[Category]:
-        return self.db.query(Category).all()
+        try:
+            return self.db.query(Category).all()
+        except SQLAlchemyError as e:
+            print("An error occurred while finding all categories:", str(e))
+            return []
 
     def update(self, id: int, **kwargs) -> Optional[Category]:
         category = self.find(id)
         if category:
-            for key, value in kwargs.items():
-                setattr(category, key, value)
-            self.db.commit()
-            self.db.refresh(category)
-        return category
+            try:
+                for key, value in kwargs.items():
+                    setattr(category, key, value)
+                self.db.commit()
+                self.db.refresh(category)
+                return category
+            except SQLAlchemyError as e:
+                self.db.rollback()
+                print("An error occurred while updating the category:", str(e))
+                return None
+        return None
 
     def delete(self, id: int) -> bool:
         category = self.find(id)
         if category:
-            self.db.delete(category)
-            self.db.commit()
-            return True
+            try:
+                self.db.delete(category)
+                self.db.commit()
+                return True
+            except SQLAlchemyError as e:
+                self.db.rollback()
+                print("An error occurred while deleting the category:", str(e))
+                return False
         return False
--- repository/supplier_repository.py
+++ repository/supplier_repository.py
@@ -8,30 +8,54 @@ class SupplierRepository(Repository):
 
     def create(self, **kwargs) -> Supplier:
         supplier = Supplier(**kwargs)
-        self.db.add(supplier)
-        self.db.commit()
-        self.db.refresh(supplier)
-        return supplier
+        try:
+            self.db.add(supplier)
+            self.db.commit()
+            self.db.refresh(supplier)
+            return supplier
+        except SQLAlchemyError as e:
+            self.db.rollback()
+            print("An error occurred while creating the supplier:", str(e))
+            return supplier
 
     def find(self, id: int) -> Optional[Supplier]:
-        return self.db.query(Supplier).filter(Supplier.id == id).first()
+        try:
+            return self.db.query(Supplier).filter(Supplier.id == id).first()
+        except SQLAlchemyError as e:
+            print("An error occurred while finding the supplier:", str(e))
+            return None
 
     def find_all(self) -> List[Supplier]:
-        return self.db.query(Supplier).all()
+        try:
+            return self.db.query(Supplier).all()
+        except SQLAlchemyError as e:
+            print("An error occurred while finding all suppliers:", str(e))
+            return []
 
     def update(self, id: int, **kwargs) -> Optional[Supplier]:
         supplier = self.find(id)
         if supplier:
-            for key, value in kwargs.items():
-                setattr(supplier, key, value)
-            self.db.commit()
-            self.db.refresh(supplier)
-        return supplier
+            try:
+                for key, value in kwargs.items():
+                    setattr(supplier, key, value)
+                self.db.commit()
+                self.db.refresh(supplier)
+                return supplier
+            except SQLAlchemyError as e:
+                self.db.rollback()
+                print("An error occurred while updating the supplier:", str(e))
+                return None
+        return None
 
     def delete(self, id: int) -> bool:
         supplier = self.find(id)
         if supplier:
-            self.db.delete(supplier)
-            self.db.commit()
-            return True
+            try:
+                self.db.delete(supplier)
+                self.db.commit()
+                return True
+            except SQLAlchemyError as e:
+                self.db.rollback()
+                print("An error occurred while deleting the supplier:", str(e))
+                return False
         return False
